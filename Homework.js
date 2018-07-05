17c4228c9524ef442da234014ea9c230c220a07e
Violetta <Violettashapik@gmail.com>
2018-07-05 16:01:01 +0300

    HW 1

---
+++ Homework.js
@@ -0,0 +1,10 @@
+//Отладка, переменные: ДЗ
+// Calc
+
+var firstParameter = 5; //смысл переменной
+var secondParameter = 10; //иной комментарий, поясняющий переменную
+
+var somePartialResult = firstParameter*5; //суть переменной и формулы
+var someOtherPartialResult = secondParameter/100500; //
+
+var result = somePartialResult + someOtherPartialResult; //суть результата и переменной
